d0d7e3dc7affdc0c563340c9c1f8d25a2df4714b
Andrey Sak <andrei.sak@banuba.com>
2021-05-26 18:24:12 +0300

    1.0.16

--- BanubaOverlayEditorSDK.xcframework/ios-arm64/BanubaOverlayEditorSDK.framework/Headers/BanubaOverlayEditorSDK-Swift.h
+++ BanubaOverlayEditorSDK.xcframework/ios-arm64/BanubaOverlayEditorSDK.framework/Headers/BanubaOverlayEditorSDK-Swift.h
@@ -300,17 +300,6 @@ SWIFT_CLASS("_TtC22BanubaOverlayEditorSDK29InteractiveTestViewController")
 
 
 
-SWIFT_CLASS("_TtC22BanubaOverlayEditorSDK26OverlayAlertViewController")
-@interface OverlayAlertViewController : UIViewController
-- (void)viewWillAppear:(BOOL)animated;
-- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
-- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
-@end
-
-
-
-
-
 
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
