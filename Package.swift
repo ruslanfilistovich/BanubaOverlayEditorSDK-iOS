94a3d0237519feabb8c687a284e92e5a7d7a37ea
Gleb Markin <gleb.markin@banuba.com>
2021-09-15 16:20:24 +0300

    0.0.19

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+  name: "BanubaOverlayEditorSDK",
+  platforms: [
+    .iOS(.v11)
+  ],
+  products: [
+    .library(
+      name: "BanubaOverlayEditorSDK",
+      targets: ["BanubaOverlayEditorSDK"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "BanubaOverlayEditorSDK",
+      path: "BanubaOverlayEditorSDK.xcframework")
+  ])
